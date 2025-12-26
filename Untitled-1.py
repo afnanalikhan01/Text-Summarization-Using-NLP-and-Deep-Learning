9b105deb266bf696c14927a321d50dd59838d99b
Afnan Ali Khan <121895253+afnanalikhan01@users.noreply.github.com>
2025-12-26 09:16:08 +0530

    Delete Untitled-1.py

--- Untitled-1.py
+++
@@ -1,8 +0,0 @@
-fruits = ["Apple", "Mango", "Grapes","Orange","Papaya"]
-import numpy as np
-
-fruits = ["Apple", "Mango", "Grapes","Orange","Papaya","Kiwi"]
-
-newarr = fruits.reshape(2, 3)
-
-print(newarr)
